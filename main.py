2b3cf187494341c93f7d0a9566f4ce8b4394d773
joecus1 <122283813+joecus1@users.noreply.github.com>
2023-08-19 22:22:19 +0000

    twitter api broken

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from facebook_scraper import get_posts
 
 for post in get_posts('nintendo', pages=3):
   print(post['text'][:50])
+
+
+# twitter api seems to be broken rn, will look into fixing later
